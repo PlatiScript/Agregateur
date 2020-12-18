a67691e93871b1c85d30683b7cd2b923d58c4888
Gitea <gitea@fake.local>
2020-12-18 16:49:06 +0100

    Car reservation

--- src/models/cars/cars.router.ts
+++ src/models/cars/cars.router.ts
@@ -11,6 +11,7 @@ import auth from '../../middleware/Auth';
  */
 
 export const carsRouter = express.Router();
+
 /**
  * @swagger
  * /cars:
@@ -69,6 +70,70 @@ carsRouter.get('/', auth, async (req, res) => {
         description: element.description,
         dailyPrice: element.dailyPrice,
         company: "Team BS Car"
+    }
+    }))];
+
+    res.status(200).send(carArrayReturned);
+
+  } catch (error) {
+    res.sendStatus(404);
+  }
+});
+
+/**
+ * @swagger
+ * /cars/reservations:
+ *   get:
+ *     security:
+ *       - bearerAuth: []
+ *     description: Return all reservations
+ *     produces:
+ *       - application/json
+ *     tags:
+ *       - Cars
+ *     responses:
+ *       200:
+ *         description: reservations
+ */
+carsRouter.get('/reservations', auth, async (req, res) => {
+  try {
+    let carArrayReturned = [];
+
+    const carArray1  = await axios.get('https://webservicecar.herokuapp.com/rentals/all', {
+      headers: {
+        'Api-Token': `ZPDpXWyKDeavzEDXtMHip89eGN9gSuRzasoDrTc9vKo27YIxJ9`
+      }
+    });
+
+    const dataToken = await axios.post('https://mysterious-eyrie-25660.herokuapp.com/users/login',
+      {
+        'user': 'admin',
+        'password': 'admin'
+      });
+
+    const carArray2 = await axios.get('https://mysterious-eyrie-25660.herokuapp.com/reservations', {
+      headers: {
+        'Authorization': `Bearer ` + dataToken.data.token
+      }
+    })
+
+    carArrayReturned = carArray1.data._embedded.rentalList.map((element : any) => {
+      return {
+          id: element.id,
+          carId: element.car.id,
+          begin: element.departureDate,
+          end: element.arrivalDate,
+          company: "YannigJeremAuto"
+      }
+    });
+
+    carArrayReturned = [...carArrayReturned, ...(carArray2.data.map((element : any) => {
+      return {
+        id: element.id,
+          carId: element.carId,
+          begin: element.begin,
+          end: element.end,
+          company: "Team BS Car"
     }
     }))];
 
